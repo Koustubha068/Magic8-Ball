b96ebf8b1d6fd9aaf4c804eca90339b3af49e0e0
Koustubha068 <KoustuK@gmail.com>
2023-12-15 11:21:51 -0500

    add 8 ball js

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
-function shakeMagic8Ball(){
+function shakeMagic8Ball() {
     const question = prompt('Ask the Magic 8-Ball a question:');
-    if(question === null){
+    //if prompt is cancelled or has no value, what happens?
+    if (question === null) {
         document.getElementById('response-text').innerText = 'U MUST ASK A QUESTION!'
-        document.getElementById('response-text').classList ='text-danger'
-        document.getElementById('response-image').src ='#BAD/BROKENIMAGE' //fix this later
+        document.getElementById('response-text').classList = 'text-danger'
+        document.getElementById('response-image').src = '#BAD/BROKENIMAGE' //fix this later
+        return
     }
 
-    if(!question.trim()){
+    if (!question.trim()) {
         alert('Please enter a valid question')
         return
     }
+    const randomNumber = Math.floor(math.random()*8);
+    let answer,image,color;
+
+    switch(randomNumber){
+        case 0:
+            answer="Yes"
+            image = "yes-image.jpg"//change this****
+            color ="text-success"
+            break
+        case 1:
+            answer="No"
+            image = "no-image.jpg"//change this****
+            color ="text-secondary"
+            break
+        case 3:
+            answer="Maybe"
+            image = "Maybe-imgage.jpg"//change this****
+            color ="text-warning"
+            break
+        case 4:
+            answer="Ask Again"
+            image = "Ask-Again-image.jpg"//change this****
+            color ="text-muted"
+            break
+        case 5:
+            answer="Don't Count on It"
+            image = "Don't-count-on-it-image.jpg"//change this****
+            color ="text-danger"
+            break
+        case 6:
+            answer="Most Likely"
+            image = "Most-Likely-image.jpg"//change this****
+            color ="text-primary"
+            break
+        case 7:    
+            answer="Definitely NOT!"
+            image = "Definetly-not-image.jpg"//change this****
+            color ="text-danger"
+            break
+    }
+    
+
+
 }
